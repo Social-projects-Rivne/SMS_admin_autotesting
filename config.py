d0df22b4d2a886954337e3dce238e0ebf43035c2
Oleksiy <lofa_job@ukr.net>
2015-04-16 17:08:20 +0300

    Init GIT repository

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/database'
+
+CSRF_ENABLED = True
+SECRET_KEY = ''
