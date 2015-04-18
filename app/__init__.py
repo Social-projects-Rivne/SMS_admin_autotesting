aded3fdb61f67c5c8ebee8cd32917f091ea30c17
Oleksiy <lofa_job@ukr.net>
2015-04-18 13:44:23 +0300

    implemented basics templates for admin service page

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,5 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+from app.views import users
--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/database'
-
 CSRF_ENABLED = True
 SECRET_KEY = ''
