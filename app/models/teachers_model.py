ebcd28b572b05cd666e7ba4666b2931ab2db0c43
Hennadii Feshchuk <genichf@gmail.com>
2015-04-21 22:43:03 +0300

    implement two methods get_all_teachers get_all_roles

--- app/models/teachers_model.py
+++ app/models/teachers_model.py
@@ -6,7 +6,7 @@ from app.utils.MySQLORM import MySQLORM
 class TeachersModel(object):
 	"""Class for Teachers Entity"""
 	def __init__(self):
-		self.get_orm('localhost', 'root', 'password', 'SMSDB')
+		self.get_orm('localhost', 'root', 'Bolo78G', 'SMSDB')
 
 	def get_orm(self, host, username, password, db):
 		self.orm = MySQLORM()
@@ -15,18 +15,18 @@ class TeachersModel(object):
 
 	def get_teacher_by_id(self, id_):
 		'''Method for getting dict of one teacher by id'''
-		return self.orm.get(teachers_table, {'id' : '%s'} % id_)
+		return self.orm.get('Teachers', {'id' : '%s'} % id_)
 
 	def get_all_teachers(self):
 		'''Method for getting list of all teachers'''
-		sql = 'SELECT id, name, login, password, email from teachers_table \
-		INNER JOIN roles_table ON roles_table.id = teachers_table.role_id \
-		INNER JOIN schools_table ON schools_table.id = teachers_table.teacher_id'
+		sql = 'SELECT t.id id, t.name name, t.login login, t.password password, t.email email, r.role_name role_name, s.name school_name FROM Teachers t \
+		INNER JOIN Roles r ON r.id = t.role_id \
+		INNER JOIN Schools s ON s.id = t.school_id'
 		return self.orm.mysql_do(sql)
 
 	def add_teacher(self, **kwargs):
 		'''Method for adding new teacher, **kwarg - dictionary, for example: {'name':'Sasha', 'login' : 'sasha_login', ... e.t.c.}'''
-		sqlresult = self.orm.insert(table, 'name', 'login', 'passwd', kwargs['name'], kwargs['login'],kwargs['password'])
+		sqlresult = self.orm.insert('Teachers', 'name', 'login', 'passwd', kwargs['name'], kwargs['login'],kwargs['password'])
         # TO DO
 
 	def edit_teacher(self):
@@ -39,6 +39,8 @@ class TeachersModel(object):
 		pass
 
 	def get_all_roles(self):
-		pass
+		'''Method for getting list of dict all roles'''
+		sql = 'SELECT id, role_name FROM Roles'
+		return self.orm.mysql_do(sql)
 
 
