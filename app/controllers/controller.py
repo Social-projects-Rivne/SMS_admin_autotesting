5a25865798259a805ff30fc073bae83c74f83243
Pasha <zhmakp@gmail.com>
2015-04-21 10:24:42 +0300

    correction controller

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from app.models import AdminModel
+from app.view import View
+from app.models import teachers_model
 
 
 class AdminController(object):
 
-    """docstring for AdminController"""
+    """AdminController - user controller"""
 
     def __init__(self):
         self.model = AdminModel()
@@ -15,18 +16,25 @@ class AdminController(object):
         """correct = true else false"""
         self.message = dict()
         # regex pattern
-        email_pattern = """^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}$"""
+        email_pattern = "^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}$"
+        name_pattern = "^([A-Z])\w+\s([A-Z])\w+$"
+        login_pattern = "^[A-Za-z0-9]+$"
         # check data
-        if not kwargs.get('name'):
+        if not kwargs.get('name') or not re.match(name_pattern, kwargs.get('name')):
             self.message['name'] = 'Invalid name'
+
         if not kwargs.get('role'):
             self.message['role'] = 'Invalid role'
+
         if not kwargs.get('password'):
             self.message['password'] = 'Invalid password'
-        if not kwargs.get('login'):
+
+        if not kwargs.get('login') or not re.match(login_pattern, kwargs.get('login')):
             self.message['login'] = 'Invalid login'
+
         if not kwargs.get('school'):
             self.message['school'] = 'Invalid school'
+            
         if not re.match(email_pattern, kwargs.get('email')):
             self.message['email'] += 'Invalid email address'
         # If validate return true
@@ -35,11 +43,11 @@ class AdminController(object):
         else: 
             return True
     
-    def get_index():
+    def get_index(self):
         """return index.html"""
         return self.view.render_index()
 
-    def get_error404():
+    def get_error404(self):
         """error 404"""
         return self.view.render_error()
 
@@ -58,10 +66,10 @@ class AdminController(object):
         else:
             return self.view.add_user_err(self.message)
 
-    def get_view_all():
+    def get_view_all(self):
         """view => user_add.html get"""
         #get data from db
-        self.data = self.model.get_all()
+        self.data = self.model.get_all_teachers()
         #render page with all users
         return self.view.render_list_users(self.data)
 
