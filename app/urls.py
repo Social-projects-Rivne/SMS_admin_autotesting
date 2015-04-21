90bef6bb22269a7a7d66bca62c2b5b94fd55961a
Oleksiy <lofa_job@ukr.net>
2015-04-21 12:08:23 +0300

    modify urls.py with using controller

--- app/urls.py
+++ app/urls.py
@@ -1,31 +1,34 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request
 
 from app import app
+from app.controllers.controller import AdminController
+
+
+controller = AdminController()
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return controller.get_index()
 
 
 @app.route('/users_list')
 def users_list():
-    context = {}
-
-    return render_template('users_list.html', context=context)
+    return controller.get_view_all()
 
 
 @app.route('/user_add', methods=['GET', 'POST'])
 def user_add():
     roles = {}
     errors = {}
-
     return render_template('user_add.html', roles=roles, errors=errors)
+    #if request.method == 'POST':
+    #    return controller.get_view_add_get()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+    return controller.get_error404()
