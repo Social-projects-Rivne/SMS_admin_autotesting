665399715e6ac72ee47c033c1cee5a0ef3dc8b69
Hennadii Feshchuk <genichf@gmail.com>
2015-04-22 23:59:50 +0300

    Add routes for schools and subjects

--- app/urls.py
+++ app/urls.py
@@ -51,3 +51,49 @@ def page_not_found(error):
     """Render 404 error page"""
 
     return controller.get_error404()
+
+#---Routes for schools----
+@app.route('/schools_list')
+def schools_list():
+    """Render page with schools list"""
+    return controller.get_view_all_schools()
+
+@app.route('/school_add', methods=['GET', 'POST'])
+def school_add():
+    """Render page with school add"""
+    return controller.get_view_add_school()
+
+@app.route('/school_remove/', methods=['GET', 'POST'])
+@app.route('/school_remove/<int:id_>', methods=['GET', 'POST'])
+def school_remove(id_):
+    """Render page with school_remove"""
+    return controller.remove_school(id_=id_)
+
+@app.route('/school_upgrade/', methods=['GET', 'POST'])
+@app.route('/school_upgrade/<int:id_>', methods=['GET', 'POST'])
+def school_upgrade(id_):
+    """Return update school form"""
+    return controller.update_school(id_=id_)
+
+#---Routes for subjects----
+@app.route('/subjects_list')
+def subjects_list():
+    """Render page with subjects list"""
+    return controller.get_view_all_subjects()
+
+@app.route('/subject_add', methods=['GET', 'POST'])
+def subject_add():
+    """Render page with subject add"""
+    return controller.get_view_add_subject()
+
+@app.route('/subject_remove/', methods=['GET', 'POST'])
+@app.route('/subject_remove/<int:id_>', methods=['GET', 'POST'])
+def subject_remove(id_):
+    """Render page with subject_remove"""
+    return controller.remove_subject(id_=id_)
+
+@app.route('/subject_upgrade/', methods=['GET', 'POST'])
+@app.route('/subject_upgrade/<int:id_>', methods=['GET', 'POST'])
+def subject_upgrade(id_):
+    """Return update subject form"""
+    return controller.update_subject(id_=id_)
