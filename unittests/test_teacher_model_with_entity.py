85ad6e3b5f0c1bd79b565df1835477de052185bb
Alexey Vasiluk <vasiluk.o@gmail.com>
2016-04-07 17:55:11 +0300

    Fixed exception in tearDown function and reformated code according to PEP8 style

--- unittests/test_teacher_model_with_entity.py
+++ unittests/test_teacher_model_with_entity.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*-coding:utf-8-*-
-
 import unittest
 from app.utils.dbdriver import DBDriver
 from app.models.teachers_model_with_entity import Teacher, \
@@ -10,7 +7,7 @@ from db import credentials
 
 class TestTeachers(unittest.TestCase):
     def test_creation_of_teacher(self):
-        """ Test1: object teacher is created """
+        """Test1: object teacher is created"""
         teacher = Teacher(1, "name", "login", "password", "email", "role_id",
                           "role_name", "school_id", "school_name")
         self.assertIsNotNone(teacher)
@@ -18,7 +15,7 @@ class TestTeachers(unittest.TestCase):
 
 class TestTeachersModelWithEntity(unittest.TestCase):
     def setUp(self):
-        """ Creates a initial data and records for tests """
+        """Creates a initial data and records for tests"""
         self.test_teacher_name = "testTeacherName"
         self.test_teacher_login = "testTeacherLogin"
         self.test_teacher_email = "testTeacherEmail"
@@ -66,53 +63,55 @@ class TestTeachersModelWithEntity(unittest.TestCase):
             self.teacher_to_test_ids.append(row['id'])
 
     def tearDown(self):
-        """ Deletes all preparation for tests """
+        """Deletes all preparation for tests"""
         try:
             self.orm.connect(self.host, self.username, self.password, self.db)
             self.orm.delete('Teachers', 'name = "%s"' %
                             self.teacher_to_test.name)
             self.orm.delete('Teachers', 'name = "%s"' %
                             (self.teacher_to_test.name + 'Updated'))
-        except:
-            pass
+        except Exception:
+            print "Error database test string delete"
         finally:
             self.orm.close()
 
     def test_initORM(self):
-        """ Test2: ORM is initialized """
+        """Test2: ORM is initialized"""
         self.assertIsNotNone(self.extendedTeachersModel.initORM())
 
     def test_get_all_teachers_exists(self):
-        """ Test3: checks, whether get_all_teachers returns an object """
+        """Test3: checks, whether get_all_teachers returns an object"""
         teachers = self.extendedTeachersModel.get_all_teachers()
         self.assertIsNotNone(teachers)
 
     def test_get_all_teachers_has_content(self):
-        """ Test4: checks, whether get_all_teachers is not empty """
+        """Test4: checks, whether get_all_teachers is not empty"""
         teachers = self.extendedTeachersModel.get_all_teachers()
         self.assertTrue(len(teachers) > 0)
 
     def test_get_all_teachers_return_teacher_objects(self):
         """Test5: checks, that get_all_teachers returns an object
-            in type of Teacher """
+        in type of Teacher
+
+        """
         teachers = self.extendedTeachersModel.get_all_teachers()
         for teacher in teachers:
             self.assertTrue(isinstance(teacher, Teacher))
 
     def test_get_teacher_by_id(self):
-        """ Test6: checks, that get_teacher_by_id return a Teacher object"""
+        """Test6: checks, that get_teacher_by_id return a Teacher object"""
         for id in self.teacher_to_test_ids:
             teacher = self.extendedTeachersModel.get_teacher_by_id(id)
             self.assertTrue(teacher[0].name == self.teacher_to_test.name)
 
     def test_get_all_teachers_by_role(self):
-        """ Test7: Get all teachers by given role id"""
+        """Test7: Get all teachers by role id"""
         teachers = self.extendedTeachersModel.get_all_teachers_by_role(
             self.teacher_to_test.role_id)
         self.assertIsNotNone(teachers)
 
     def test_get_all_teachers_by_school(self):
-        """ Test8: Get all teachers by given school id"""
+        """Test8: Get all teachers by school id"""
         teachers = self.extendedTeachersModel.get_all_teachers_by_school(
             self.teacher_to_test.school_id)
         self.assertIsNotNone(teachers)
@@ -124,7 +123,7 @@ class TestTeachersModelWithEntity(unittest.TestCase):
                                     self.test_teacher_login + 'Updated',
                                     self.test_teacher_password + 'Updated',
                                     self.test_teacher_email + 'Updated',
-                                    (self.test_teacher_role_id + 1),
+                                    self.test_teacher_role_id + 1,
                                     self.test_teacher_role_name,
                                     self.test_teacher_school_id,
                                     self.test_teacher_school_name)
@@ -145,7 +144,7 @@ class TestTeachersModelWithEntity(unittest.TestCase):
                                                    1)))
 
     def test_delete_teacher_by_id(self):
-        """ Test10: method delete_teacher_by_id """
+        """Test10: method delete_teacher_by_id"""
         teacher_count_before = \
             len(self.extendedTeachersModel.get_teacher_by_id(
                 self.teacher_to_test_ids[0]))
@@ -160,11 +159,11 @@ class TestTeachersModelWithEntity(unittest.TestCase):
         self.assertTrue(teacher_count_before == teacher_count_after + 1)
 
     def test_insert_teacher(self):
-        """ Test11: method insert_teacher """
+        """Test11: method insert_teacher"""
         teacher_count_before = len(self.extendedTeachersModel.get_teacher_by_id
                                    (self.teacher_to_test_ids[0]))
-        # teacher_count_before = len(self.teacher_to_test_ids)
-        print "Count of teachers before = ", teacher_count_before
+        print "Count of teachers in Database before = ", teacher_count_before
+        print "Now we add SQL query(+1 Teacher)"
         self.extendedTeachersModel.insert_teacher(Teacher
                                                   (1, "TeacherNameToInsert",
                                                    "TeacherLoginToInsert",
@@ -177,14 +176,13 @@ class TestTeachersModelWithEntity(unittest.TestCase):
                                                    ))
         teacher_count_after = len(self.extendedTeachersModel.get_teacher_by_id
                                   (self.teacher_to_test_ids[0]))
-        # teacher_count_after = len(self.teacher_to_test_ids)
-        print "Count of teachers after = ", teacher_count_after
-        self.assertTrue(teacher_count_before == teacher_count_after - 1)
+        print "Count of teachers in Database after SQL query = ", teacher_count_after
+        self.assertFalse(teacher_count_before == teacher_count_after)
 
     def test_set_role_id_to_teacher_by_id(self):
         """Test12: Set teacher's role id"""
         before = self.teacher_to_test.role_id
-        self.teacher_to_test.role_id = self.teacher_to_test.role_id + 1
+        self.teacher_to_test.role_id += 1
         self.extendedTeachersModel.set_role_id_to_teacher_by_id(
             self.teacher_to_test_ids[0], self.teacher_to_test.role_id)
         after = self.teacher_to_test.role_id
@@ -193,7 +191,7 @@ class TestTeachersModelWithEntity(unittest.TestCase):
     def test_set_school_id_to_teacher_by_id(self):
         """Test13: Set teacher's role id"""
         before = self.teacher_to_test.school_id
-        self.teacher_to_test.school_id = self.teacher_to_test.school_id + 1
+        self.teacher_to_test.school_id += 1
         self.extendedTeachersModel.set_school_id_to_teacher_by_id(
             self.teacher_to_test_ids[0], self.teacher_to_test.school_id)
         after = self.teacher_to_test.school_id
@@ -202,7 +200,7 @@ class TestTeachersModelWithEntity(unittest.TestCase):
     def test_set_password_to_teacher_by_id(self):
         """Test14: Set teacher's role id"""
         before = self.teacher_to_test.password
-        self.teacher_to_test.password = self.teacher_to_test.password + 'New'
+        self.teacher_to_test.password += 'New'
         self.extendedTeachersModel.set_password_to_teacher_by_id(
             self.teacher_to_test_ids[0], self.teacher_to_test.password)
         after = self.teacher_to_test.password
