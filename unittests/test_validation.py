e3a24c5497859f5a3e8189fb41c9efed03c5cea8
dmitriivasiluk <dmytrovasyliuk96@gmail.com>
2016-04-11 09:23:30 +0300

    Improved unittest for validation.py

--- unittests/test_validation.py
+++ unittests/test_validation.py
@@ -4,17 +4,67 @@ from app.utils.validation import Validate
 
 class TestValidation(unittest.TestCase):
 	
-	def test_creation_of_validator(self):
-		""" Creation of object typeof Validate """
-		validator = Validate()
-		self.assertIsInstance(validator, Validate)
+	def setUp(self): 
+		"""Preparation"""
+    	
+		self.validator = Validate()
 	
-	validator = Validate()	
-	def test_check_email_correct(self):
-		""" Check if correct email matched """
+	def test_check_email_positive(self):
+		"""Check if correct email matched"""
+		
 		str_to_test = "alex@gmail.com"
-		result = validator.check_email(str_to_test)
+		result = self.validator.check_email(str_to_test)
+		self.assertIsInstance(result, object)
+
+	def test_check_email_negative(self):
+		"""Check if wrong email matched"""
+		
+		str_to_test = "alexgmИЛail.com"
+		result = self.validator.check_email(str_to_test)
 		self.assertIsNone(result)
+	
+	def test_check_cyrrilic_negative(self):
+		"""Check if matched non-cyrillic text for cyrillik pattern"""
+		
+		str_to_test = "sdaskfnsajkfnas"
+		result = self.validator.check_cyrillic(str_to_test)
+		self.assertIsNone(result)
+
+	def test_check_cyrrilic_positive(self):
+		"""Check if matched cyrillic text for cyrillik pattern"""
+		
+		str_to_test = u"Кирилиця для тесту№1Є"
+		result = self.validator.check_cyrillic(str_to_test)
+		self.assertIsInstance(result, object)
+
+	def test_check_name_positive(self):
+		"""Check if matched correct name"""
+		
+		str_to_test = u"Красавчік Kewa Аполонович"
+		result = self.validator.check_name(str_to_test)
+		self.assertIsInstance(result, object)
+
+	def test_check_name_negative(self):
+		"""Check if matched non correct name"""
+		
+		str_to_test = u"Зюганов Genadii 000"
+		result = self.validator.check_name(str_to_test)
+		self.assertIsInstance(result, object)
+
+	def test_check_login_positive(self):
+		"""Check if matched correct login"""
+		
+		str_to_test = "nagibator2000"
+		result = self.validator.check_login(str_to_test)
+		self.assertIsInstance(result, object)
+
+	def test_check_login_negative(self):
+		"""Check if matched non correct login"""
+		
+		str_to_test = "dsad?+-12"
+		result = self.validator.check_login(str_to_test)
+		self.assertIsNone(result)
+	
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
