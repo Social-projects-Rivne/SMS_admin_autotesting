71f5a1193e70328be056a675d5035fd74a924642
alexbass16 <alexjust2014@gmail.com>
2016-04-12 00:17:34 +0300

    code cleaning for DEMO

--- unittests/test_role.py
+++ unittests/test_role.py
@@ -12,20 +12,28 @@ class TestRolesModel(unittest.TestCase):
         self.roles = RolesModel()
 
     def test_initORM(self):
-        """Check that function is not empty"""
+        """ Check that function is not empty """
+        
         orm = self.roles.initORM()
         self.assertIsNotNone(orm)
+        
+    def test_initORM_isinstance(self):
+        """ Check that function is object of DBDriver """
+        
+        orm = self.roles.initORM()
         self.assertIsInstance(orm, DBDriver)
 
     def test_get_all_roles(self):
-        """Test, that checks, whether get_all_roles
-        return an object"""
+        """ Test, that checks, whether get_all_roles
+        return an object """
+        
         all_roles = self.roles.get_all_roles()
         self.assertEqual(all_roles[0]['id'], 1)
         self.assertEqual(all_roles[1]['role_name'], u'Завуч')
 
     def test_select_roles_query(self):
-        """Test, that checks select_roles_query"""
+        """ Test, that checks select_roles_query """
+        
         self.assertEqual(RolesModel.select_roles_query, \
                          "SELECT id, role_name FROM Roles")
 
