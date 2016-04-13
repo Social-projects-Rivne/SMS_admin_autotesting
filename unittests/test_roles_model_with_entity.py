3b7ec2b8df3febbdf707f07e15d943cfaf9537b1
dmitriivasiluk <dmytrovasyliuk96@gmail.com>
2016-04-13 09:21:01 +0300

    Cleaned up test_roles_model_with_entity.py according to PEP8

--- unittests/test_roles_model_with_entity.py
+++ unittests/test_roles_model_with_entity.py
@@ -8,62 +8,58 @@ from app.models.roles_model_with_entity import Role, \
 
 
 class TestRole(unittest.TestCase):
+    """ Class to test class Role """
     def test_creation_of_role(self):
-        
-        """Basic smoke test: object role is created"""
-        
-        role = Role(2, u'Завуч')
-        self.assertIsInstance(role, Role)
+        """ Basic smoke test: object role is created """
+        my_test_role = Role(2, u'Завуч')
+        self.assertIsInstance(my_test_role, Role)
 
 
 class TestExtendedRolesModel(unittest.TestCase):
-   
+    """ Class to test ExtendedRolesModel class """
     def setUp(self):
-        
-        """Preparation"""
-        
+        """ Preparation """
         self.test_role_name = u"Завуч"
         self.test_role_id = 2
-        self.extendedRolesModel = ExtendedRolesModel()
+        self.erm = ExtendedRolesModel()
         self.role_to_test = Role(self.test_role_id, self.test_role_name)
 
         from db import credentials
         self.host = credentials[0]
         self.username = credentials[1]
         self.password = credentials[2]
-        self.db = credentials[3]
+        self.dbname = credentials[3]
 
         self.orm = DBDriver()
-        self.orm.connect(self.host, self.username, self.password, self.db)
-        
+        self.orm.connect(self.host, self.username, self.password, self.dbname)
 
     def tearDown(self):
-        """Fixture that deletes all preparation for tests"""
+        """ Fixture that deletes all preparation for tests """
         self.orm.close()
 
-    def test_initORM(self):
-        """Basic smoke test: ORM is initialized"""
-        
-        self.assertIsNotNone(self.extendedRolesModel.initORM())
+    def test_init_orm(self):
+        """ Basic smoke test: ORM is initialized """
+        self.assertIsNotNone(self.erm.initORM())
 
     def test_get_all_roles(self):
-        """Get all roles"""
-        
-        roles = self.extendedRolesModel.get_all_roles()
+        """ Test to check if get_all_roles returns list of 3 elements """
+        roles = self.erm.get_all_roles()
         self.assertTrue(len(roles) == 3)
 
     def test_get_role_by_id(self):
-        """Get role by given id"""
-        
-        roles_by_id = self.extendedRolesModel.get_role_by_id(self.test_role_id)
+        """ Test to check whether get_role_by_id \
+         returns object with id that set"""
+        roles_by_id = self.erm.get_role_by_id(self.test_role_id)
         self.assertTrue(roles_by_id[0].role_name == self.test_role_name)
 
     def test_create_list_from_dbresult(self):
-        """Check if func creates list"""
-        
-        role = self.extendedRolesModel._get_roles('WHERE id=%d' % (self.test_role_id))
+        """ Test to check whether create_list_from_dbresult \
+        returns list with objects """
+        role = \
+        self.erm._get_roles('WHERE id=%d' % (self.test_role_id))
         self.assertIsInstance(role[0], Role)
 
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
+
