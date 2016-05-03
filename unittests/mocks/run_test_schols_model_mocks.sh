3a87531feaa27a0ca175f8574c3631fad4bc9f84
Alexey Boris <bo-a@yandex.ru>
2016-05-03 14:49:22 +0300

    Improved scripts to run tests.  Changes to be committed: 	modified:   unittests/mocks/run_test_schols_model_mocks.sh 	modified:   unittests/run_test_controller.sh 	modified:   unittests/run_test_schools_model.sh

--- unittests/mocks/run_test_schols_model_mocks.sh
+++ unittests/mocks/run_test_schols_model_mocks.sh
@@ -1,6 +1,9 @@
-cd ../../
+#/bin/bash
+#cd ../../
 python -m unittests.mocks.test_schools_model_with_entity_mocks
+read -n1 -r -p 'Press any key...' key 
 coverage run -m unittests.mocks.test_schools_model_with_entity_mocks
 coverage report -m unittests/mocks/test_schools_model_with_entity_mocks.py
+read -n1 -r -p 'Press any key...' key 
 coverage html unittests/mocks/test_schools_model_with_entity_mocks.py
-
+firefox htmlcov/app_models_schools_model_with_entity_py.html 
--- unittests/run_test_schools_model.sh
+++ unittests/run_test_schools_model.sh
@@ -1,6 +1,9 @@
-cd ../
+#/bin/bash
+#cd ../
 python -m unittests.test_schools_model_with_entity
+read -n1 -r -p 'Press any key...' key 
 coverage run -m unittests.test_schools_model_with_entity
 coverage report -m unittests/test_schools_model_with_entity.py
+read -n1 -r -p 'Press any key...' key 
 coverage html unittests/test_schools_model_with_entity.py
-
+firefox htmlcov/app_models_schools_model_with_entity_py.html 
