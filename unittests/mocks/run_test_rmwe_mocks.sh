930d2f935e81cea997504ef774fabf2fd0fb5356
dmitriivasiluk <dmytrovasyliuk96@gmail.com>
2016-05-04 07:52:35 +0300

    Implemented runfile for roles_model_with_entity test on mocks

--- unittests/mocks/run_test_rmwe_mocks.sh
+++ unittests/mocks/run_test_rmwe_mocks.sh
@@ -3,4 +3,4 @@ python -m unittests.mocks.test_roles_model_with_entity_mocks
 coverage run -m unittests.mocks.test_roles_model_with_entity_mocks
 coverage report -m unittests/mocks/test_roles_model_with_entity_mocks.py
 coverage html unittests/mocks/test_roles_model_with_entity_mocks.py
-
+firefox htmlcov/app_models_roles_model_with_entity_py.html
